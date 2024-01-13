a151629f8fcfc5cf87e82ea5b3369ccc442919de
Nicolas Wender Fontes Queiroz <nicolas.w3nder@gmail.com>
2024-01-13 09:36:43 -0300

    feat: adicionando socket.io

--- backend/servidor.js
+++ backend/servidor.js
@@ -2,12 +2,24 @@ import express from 'express'
 import url from 'url'
 import path from 'path'
 import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 const porta = process.env.porta || 3000
 
 const caminhoAtual = url.fileURLToPath(import.meta.url)
 const diretorioPublico = path.join(caminhoAtual, '../..', 'src')
+
 app.use(express.static(diretorioPublico))
 
-app.listen(porta, () => console.log(`Servidor rodando em  http://localhost:${porta}`))
+const servidorHttp = http.createServer(app)
+
+servidorHttp.listen(porta, () =>
+  console.log(`Servidor rodando em  http://localhost:${porta}`)
+)
+
+const io = new Server(servidorHttp)
+
+io.on('connection', socket => {
+  console.log('Conectado ao socket')
+})
